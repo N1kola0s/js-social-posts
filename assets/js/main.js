1d0a04ec978b1074ed46ffcd8f7f4d266fbf176d
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-12 17:16:04 +0200

    add card in js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,28 +30,67 @@ Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e inc
 //Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
 const posts = [
 {
-    id_post:0001,
+    id_post:1,
     user_name: 'Phil Mangione',
     user_image:'https://picsum.photos/200/300',
     date:'05-27-2021',
-    text:'testo3',
+    text:'Questo è un testo di prova del primo post',
     post_image: 'https://picsum.photos/200/300',
     like:40
 
 },
 {
-    id_post:0002,
+    id_post:2,
     user_name: 'Sofia Perlari',
     user_image: 'https://picsum.photos/200/300',
     date: '07-20-2022',
-    text:'testo2',
+    text:'Questo è un testo di prova del secondo post',
     post_image: 'https://picsum.photos/200/300',
     like: 30
 
 }
 ];
 
-console.log(posts);
+/* console.log(posts); */
 
 //Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 
+const socialElement = document.querySelector(".post")
+
+
+posts.forEach((post, i) =>{
+    post = posts[i];
+
+    /* console.log(typeof post);  */
+
+   /*  console.log(post.id_post)
+    console.log(post.user_name)
+    console.log(post.user_image)
+    console.log(post.date)
+    console.log(post.text)
+    console.log(post.post_image)
+    console.log(post.like)
+    console.log("_____________________")  */
+
+
+    let card = document.createElement("div");
+
+    //stampo il contenuto al suo interno
+       /*  card.innerHTML =  
+        `
+        <div class="card">
+            <div class="name">${post.user_name}</div> 
+            <div class="date">${post.date}</div>
+            <img src="${post.user_image}" alt="profile-image" >
+            <div class="text">${post.text}</div>
+            <img src="${post.post_image}" alt="post-image">
+            <div>Mi piace</div>
+            <div class="like">${post.like}</div>
+        </div>  
+      
+        `;  */
+
+    //appendo quanto creato all'interno di socialElement nella dom
+    socialElement.append(card);
+   
+})
