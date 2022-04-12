887655575d251da2b4c708c171820cc44a3b7a75
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-12 15:59:58 +0200

    add milestone 1 in js code

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/* Descrizione:
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
+Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post. 
+
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+- id del post, numero progressivo da 1 a n
+- nome autore,
+- foto autore,
+- data in formato americano (mm-gg-yyyy),
+- testo del post,
+- immagine (non tutti i post devono avere una immagine),
+-numero di likes.
+
+Non è necessario creare date casuali
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+
+Milestone 2
+
+Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+Milestone 3
+
+Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like. */
+
+
+//Creiamo il nostro array di oggetti che rappresentano ciascun post. 
+
+//Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+const posts = [
+{
+    id_post:0001,
+    user_name: 'Phil Mangione',
+    user_image:'https://picsum.photos/200/300',
+    date:'05-27-2021',
+    text:'testo3',
+    post_image: 'https://picsum.photos/200/300',
+    like:40
+
+},
+{
+    id_post:0002,
+    user_name: 'Sofia Perlari',
+    user_image: 'https://picsum.photos/200/300',
+    date: '07-20-2022',
+    text:'testo2',
+    post_image: 'https://picsum.photos/200/300',
+    like: 30
+
+}
+];
+
+console.log(posts);
+
+//Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
